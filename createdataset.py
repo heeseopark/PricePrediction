e009e143f7417559070294c688b3b203cfa3e1e1
heeseopark <heeseopark@korea.ac.kr>
2023-05-16 19:15:19 +0900

    0516 commit

--- createdataset.py
+++ createdataset.py
@@ -1,31 +1,36 @@
 import requests
 import pandas as pd
-import torch
+from datetime import datetime, timedelta
 
-# Define the URL and the parameters
+# Define the URL
 url = 'https://api.binance.com/api/v3/klines'
 params = {'symbol': 'BTCUSDT', 'interval': '1m'}
 
-# Make the GET request
-response = requests.get(url, params=params)
-data = response.json()
+# Get today's date and yesterday's date
+today = datetime.now()
+yesterday = today - timedelta(days=1)
 
-# Convert the data to a DataFrame
-df = pd.DataFrame(data, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
+# Initialize DataFrame
+df = pd.DataFrame()
 
-# Convert the time columns to datetime
-df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
-df['Close time'] = pd.to_datetime(df['Close time'], unit='ms')
+while True:
+    # Make the GET request
+    response = requests.get(url, params=params)
+    data = response.json()
 
-# Select the relevant columns
-df = df[['Open time', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    # Convert the data to a DataFrame and append it to the existing DataFrame
+    df_temp = pd.DataFrame(data, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
+    df = pd.concat([df, df_temp], ignore_index=True)
 
-# Convert the price and volume data to floats
-for col in ['Open', 'High', 'Low', 'Close', 'Volume']:
-    df[col] = df[col].astype(float)
+    # Get the time of the earliest data point in the DataFrame
+    earliest_time = pd.to_datetime(df['Open time'].min(), unit='ms')
 
-# Convert the DataFrame to a PyTorch tensor
-tensor = torch.tensor(df[['Open', 'High', 'Low', 'Close', 'Volume']].values)
+    # If the earliest time in the DataFrame is later than yesterday, break the loop
+    if earliest_time <= yesterday:
+        break
 
-print(tensor)
-print(len(tensor))
+    # Otherwise, set the end time for the next request to be the earliest time in the DataFrame
+    params['endTime'] = int(earliest_time.timestamp() * 1000)
+
+# Save the DataFrame to a CSV file
+df.to_csv('btc_data.csv', index=False)
